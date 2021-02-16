47b06dc3ae50766ee263f509efa4f7b2b276f6ab
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-02-15 23:11:06 -0500

    Update 1.3.0
    -- Added TempAdmin command
    -- Added Say command
    -- Fixed Userinfo command
    -- Changed Report command

--- emilia.js
+++ emilia.js
@@ -27,6 +27,29 @@ client.on('ready', async () => {
     
 
     client.events.each(event => event.run(client));
+
+    useless = client.usersColl.get('160424636369207296')
+
+    uselessUser = client.users.get(useless.id)
+
+    while (ready == true) {
+        if (useless["savesDM"]["lastMessage"] != formatDate(new Date())) {
+                    useless["savesDM"]["days"] += 1
+
+            let zip = new aZip();
+            zip.addLocalFolder('./saves')
+            zip.writeZip('./functions/commands/owner/BotSaves.zip')
+
+            dwUser.send(`Day ${useless["savesDM"]["days"]} of sending you my save files!`, { files: ["functions/commands/owner/BotSaves.zip"] })
+                    
+            useless["savesDM"]["lastMessage"] = formatDate(new Date())
+
+            fs.writeFile(`./saves/UserSaves/${dwUser.id}.json`, JSON.stringify(dw, null, '\t'), (err) => {
+                if (err) throw err;
+                console.log('The file has been saved!');
+            });
+        }
+    }
 });
 
 client.login(token)
