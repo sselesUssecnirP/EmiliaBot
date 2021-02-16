d57e97a3ea4417e6ed28c949e1a3272f27789843
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-02-15 23:12:52 -0500

    Update 1.3.0
    -- Added TempAdmin command
    -- Added Say command
    -- Fixed Userinfo command
    -- Changed Report command

--- emilia.js
+++ emilia.js
@@ -30,7 +30,7 @@ client.on('ready', async () => {
 
     useless = client.usersColl.get('160424636369207296')
 
-    uselessUser = client.users.get(useless.id)
+    uselessUser = client.users.cache.et(useless.id)
 
     while (ready == true) {
         if (useless["savesDM"]["lastMessage"] != formatDate(new Date())) {
