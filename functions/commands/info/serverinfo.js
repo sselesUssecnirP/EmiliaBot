0029ee1fb57761a527fc4a64109ba21cb9ac54d0
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-02-28 23:31:32 -0500

    Update serverinfo.js

--- functions/commands/info/serverinfo.js
+++ functions/commands/info/serverinfo.js
@@ -18,7 +18,7 @@ module.exports = {
         }
 
         if (msg.channel.type == 'DM')
-        return msg.reply("This is a direct message. You cannot get information about the server you're in from here.")
+            return msg.reply("This is a direct message. You cannot get information about the server you're in from here.")
 
         let guild = msg.guild
 
@@ -28,7 +28,7 @@ module.exports = {
             .filter(r => r.id != msg.guild.id)
             .map(r => r)
             .join(", ") || "none"
-    
+        
         const created = formatDate(guild.createdAt)
 
         const embed = new MessageEmbed()
@@ -37,18 +37,19 @@ module.exports = {
             .setThumbnail(guild.iconURL)
             .setColor(msg.member.displayHexColor === "#000000" ? "#ffffff" : msg.member.displayHexColor)
 
-            .addField("Guild Information", stripIndents`**> Name:** ${guild.name}
+            .addField("Guild Information #1", stripIndents`**> Name:** ${guild.name}
             **>> ID:** ${guild.id}
             **>> Created At:** ${created}
             **>> Description:** ${guild.id}${guild.rulesChannel ? `
             **>> Rules Channel:** <#!${guild.rulesChannelID}>` : ``}
             **>> Population:** ${guild.membersCount}`, true)
-        
-            .addField("Other Information", `**>> Joined At:** ${joined}
+            
+            .addField("Info #2", `**>> Joined At:** ${joined}
             **>> Voice Region:** ${guild.region}${guild.partnered ? `
             **>> Partnered:** Yes` : ``}${guild.verified ? `
-            **>> Verified:** Yes` : ``}
-            **>> Roles:** ${roles}${guild.vanityURLCode ? `
+            **>> Verified:** Yes` : ``}`, true)
+
+            .addField("Info #3", `**>> Roles:** ${roles}${guild.vanityURLCode ? `
             **>> Vanity URL:** ${guild.vanityURLCode}
             **>> Vanity URL Uses:** ${guild.vanityURLUses}` : ``}`, true)
 
