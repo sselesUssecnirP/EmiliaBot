a8fdaf182e414b90356c7beda370b739d9c2543d
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-03-08 19:39:50 -0500

    Update subaru.js

--- functions/commands/fun/subaru.js
+++ functions/commands/fun/subaru.js
@@ -10,9 +10,13 @@ module.exports = {
     run: async (client, msg, args) => {
         
         if (msg.mentions.members.first()) {
-            let user = msg.mentions.members.first()
+            let user = msg.mentions.users.first()
 
             user.send(`**${msg.author.username}**:::`, { files: ["files/videos/iloveemilia.mp4"] })
+        } else if (!msg.mentions.users.first()) {
+            msg.author.send(``, { files: ["files/videos/iloveemilia.mp4"] })
+        } else if (!msg.mentions.members.first() && msg.mentions.users.first()) {
+            msg.reply('The user you mentioned is not in this guild.').then(m => m.delete({ timeout: 15000 }))
         }
     }
 }
