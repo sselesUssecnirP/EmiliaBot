d5520a020b02292244734e10201a9328ff8368bf
sselesUssecnirP <SkyDaButterRS@gmail.com>
2021-03-11 10:57:17 -0500

    Update subaru.js

--- functions/commands/fun/subaru.js
+++ functions/commands/fun/subaru.js
@@ -9,14 +9,14 @@ module.exports = {
     usage: "[mention]",
     run: async (client, msg, args) => {
         
-        if (msg.mentions.members.first()) {
-            let user = msg.mentions.users.first()
-
-            user.send(`**${msg.author.username}**:::`, { files: ["./files/videos/iloveemilia.mp4"] })
-        } else if (args.length == 0) {
+        if (args.length == 0) {
             msg.author.send({ files: ["./files/videos/iloveemilia.mp4"]})
-        } else if (args.length > 0 && !msg.mentions.members.first()) {
-            msg.reply('The user you mentioned is not in this guild.').then(m => m.delete({ timeout: 15000 }))
+        } else {
+            if (msg.mentions.members.first()) {
+                let user = msg.mentions.members.first()
+
+                user.send(`__**${msg.author.username}:**__`, { files: ["./files/videos/iloveemilia.mp4"]})
+            }
         }
     }
 }
